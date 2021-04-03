9d18c91e62d8e934f4e2b90a778a6ea2c4cf123a
nmd2410 <46246477+nmd2410@users.noreply.github.com>
2021-04-03 23:27:32 +0700

    call api

--- src/components/Order/OrderForm.js
+++ src/components/Order/OrderForm.js
@@ -1,13 +1,53 @@
-import { Grid } from '@material-ui/core';
-import React from 'react';
+import { Grid, InputAdornment, makeStyles, ButtonGroup, Button as MuiButton } from '@material-ui/core';
+import React, {useState, useEffect} from 'react';
 import Form from '../../layouts/Form';
 import {Input, Select, Button} from '../../controls';
+import ReplayIcon from '@material-ui/icons/Replay';
+import RestaurantMenuIcon from '@material-ui/icons/RestaurantMenu';
+import ReorderIcon from '@material-ui/icons/Reorder';
+import { createAPIEndpoint, ENDPIONTS } from "../../api/index";
 
-export default function OrderForm() {
-    const pMethod = [
-        {id : 1, title: "Cash"},
-        {id : 2, title: "Card"},
-    ]
+const pMethod = [
+    {id : 1, title: "Cash"},
+    {id : 2, title: "Card"},
+]
+const useStyles = makeStyles(theme => ({
+    adornmentText: {
+        '& .MuiTypography-root': {
+            color: '#f3b33d',
+            fontWeight: 'bolder',
+            fontSize: '1.5em'
+        }
+    },
+    submitButtonGroup: {
+        backgroundColor: '#f3b33d',
+        color: '#000',
+        margin: theme.spacing(1),
+        '& .MuiButton-label': {
+            textTransform: 'none'
+        },
+        '&:hover': {
+            backgroundColor: '#f3b33d',
+        }
+    }
+}))
+export default function OrderForm(props) {
+    const {values, errors, handleSelectChage} = props;
+    const classes = useStyles();
+    const [customerList, setCustomerList] = useState([]);
+
+    useEffect(() => {
+        createAPIEndpoint(ENDPIONTS.CUSTOMER).fetchAll()
+            .then(res => {
+                let customerList = res.data.map(item => ({
+                    id: item.customerID,
+                    title: item.customerName
+                }));
+                customerList = [{ id: 0, title: 'Select' }].concat(customerList);
+                setCustomerList(customerList);
+            })
+            .catch(err => console.log(err))
+    }, [])
     return (
         <Form>
             <Grid container>
@@ -15,27 +55,54 @@ export default function OrderForm() {
                     <Input
                         disabled
                         label="Order Number"
-                        name="orderNumber">
+                        name="orderNumber"
+                        value={values.orderNumber}
+                        InputProps={{
+                            startAdornment: <InputAdornment
+                                className={classes.adornmentText}
+                                position="start">#</InputAdornment>
+                        }}>
                     </Input>
                     <Select
                         label="Customer"
                         name="customerId"
-                        options={[
-                            { id: 1, title: 'dungnm' },
-                            { id: 2, title: 'dungnm1' },
-                            { id: 3, title: 'dungnm2' }
-                        ]}/>
+                        value={values.customerId}
+                        onChage = {handleSelectChage}
+                        options={customerList}/>
                 </Grid>
                 <Grid item xs={6}>
                     <Input
                         disabled
                         label="Grand Total"
-                        name="gTotal">
+                        value={values.gTotal}
+                        name="gTotal"
+                        InputProps={{
+                            startAdornment: <InputAdornment
+                                className={classes.adornmentText}
+                                position="start">$</InputAdornment>
+                        }}>
                     </Input>
                     <Select
                         label="Pay Method"
+                        value={values.pMethod}
                         name="pMethod"
+                        onChage = {handleSelectChage}
                         options={pMethod}/>
+                        <ButtonGroup className={classes.submitButtonGroup}>
+                            <MuiButton
+                                size="large" endIcon={<RestaurantMenuIcon />}
+                                type="submit">Submit</MuiButton>
+                            <MuiButton
+                                size="small"
+                                // onClick={resetForm}
+                                startIcon={<ReplayIcon />}
+                            />
+                        </ButtonGroup>
+                        <Button
+                            size="large"
+                            // onClick={openListOfOrders}
+                            startIcon={<ReorderIcon />}
+                        >Orders</Button>
                 </Grid>
             </Grid>
         </Form>
